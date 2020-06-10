994a30aba9222c9d7b0729af6a4b6c3382c66c62
Christian Pari <ChristianPari@outlook.com>
2020-06-09 20:52:24 -0400

    root statics and router

---
+++ routes/homeRouter.js
@@ -0,0 +1,13 @@
+const express = require('express'), // allows access to express methods and objects
+    router = express.Router(); // allows you to create route handling
+
+router.use(express.static(process.cwd() + '/public/homeStatic'));
+//^ need to specifiy the static folder for each route if using multiple routes
+
+router.get('/', (req, res) => {
+
+    res.sendFile(process.cwd() + '/public/homeStatic/index.html');
+
+});
+
+module.exports = router; // needed to use and this handling elsewhere
